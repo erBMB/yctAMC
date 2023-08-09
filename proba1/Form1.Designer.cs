2f8cb9ec78fe434ebf7f055a5fda51424ce6f1be
erBMB <reduardbumbu@gmail.com>
2023-08-09 21:53:36 +0300

    Add project files.

---
+++ proba1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace proba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load = new System.Windows.Forms.Button();
+            this.txtOffset = new System.Windows.Forms.TextBox();
+            this.txtArat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(42, 97);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(112, 36);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "btnLoad";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // txtOffset
+            // 
+            this.txtOffset.Location = new System.Drawing.Point(54, 222);
+            this.txtOffset.Name = "txtOffset";
+            this.txtOffset.Size = new System.Drawing.Size(90, 26);
+            this.txtOffset.TabIndex = 1;
+            this.txtOffset.Text = "0";
+            // 
+            // txtArat
+            // 
+            this.txtArat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArat.Location = new System.Drawing.Point(198, 48);
+            this.txtArat.Multiline = true;
+            this.txtArat.Name = "txtArat";
+            this.txtArat.Size = new System.Drawing.Size(464, 297);
+            this.txtArat.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArat);
+            this.Controls.Add(this.txtOffset);
+            this.Controls.Add(this.Load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.TextBox txtOffset;
+        private System.Windows.Forms.TextBox txtArat;
+    }
+}
+
---
+++ proba1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "sursa.txt");
+            string text = File.ReadAllText(path, Encoding.UTF8);
+            // Replace [NAME] with a variable's value
+            string codBare1 = "ana";
+            string cpn = "are";
+            string pnData = "multe";
+            string codBare2 = "Bob";
+
+            int ana = 477;
+            int are = 25;
+            int multe = 42;
+            int mere = 50;
+            int gustoase = 42;
+
+            string modifiedContent = text
+                .Replace("[codBare1]", codBare1)
+                .Replace("[cpn]", cpn)
+                .Replace("[pnData]", pnData)
+                .Replace("[codBare2]", codBare2)
+                .Replace("[ana]", (ana + int.Parse(txtOffset.Text)).ToString())
+                .Replace("[are]", (are + int.Parse(txtOffset.Text)).ToString())
+                .Replace("[multe]", (multe + int.Parse(txtOffset.Text)).ToString())
+                 .Replace("[mere]", (mere + int.Parse(txtOffset.Text)).ToString())
+                .Replace("[gustoase]", gustoase + int.Parse(txtOffset.Text).ToString());
+            txtArat.Text= modifiedContent;
+        }
+    }
+}
